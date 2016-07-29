ed449827fab0cf45eaadba95c02d1ebb58e76dd2
Alibali <Ali.Ringsted@gmail.com>
2016-07-29 20:18:49 +0200

    ContactFormAdded!

---
+++ ZxaUmbraco/ZxaUmbraco/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+using ZxaUmbraco.Models;
+
+namespace ZxaUmbraco.Controllers
+{
+    public class ContactSurfaceController: SurfaceController
+    {
+
+        public ActionResult ShowForm() {
+
+            ContactModel myModel = new ContactModel();
+            return PartialView("ContactForm", myModel);
+        }
+    
+    
+        public ActionResult HandleFormPost(ContactModel model)
+        {
+            var newComment = Services.ContentService.CreateContent(model.Name + " - " + DateTime.Now.ToString("dd-MM-yyyy HH:mm"), CurrentPage.Id, "contactFormer");
+            newComment.SetValue("emailFrom", model.Email);
+            newComment.SetValue("contactName", model.Name);
+            newComment.SetValue("contactMessage", model.Message);
+            Services.ContentService.SaveAndPublishWithStatus(newComment);
+            return RedirectToCurrentUmbracoPage();
+        }
+    
+    
+    
+    }
+            
+}
---
+++ ZxaUmbraco/ZxaUmbraco/Models/ContactModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZxaUmbraco.Models
+{
+    public class ContactModel
+    {
+        public  int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        }
+}
